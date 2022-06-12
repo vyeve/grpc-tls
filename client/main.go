32f97c3f82e707b30740ed9c1807c21eb1fea1a9
vyeve <xobpaxa@ukr.net>
2022-06-12 21:14:09 +0300

    added client part

---
+++ client/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/vyeve/grpc-ssh-server/cert"
+	"github.com/vyeve/grpc-ssh-server/models"
+
+	"google.golang.org/grpc"
+)
+
+type client struct {
+	client models.SSHClient
+	logger *log.Logger
+}
+
+func main() {
+	cl := initClient()
+	for i := 0; i < 10; i++ {
+		cl.makeRequest(i)
+		time.Sleep(time.Second * 2)
+	}
+}
+
+func initClient() *client {
+	tlsCredentials, err := cert.LoadClientTLSCredentials()
+	if err != nil {
+		log.Fatal("cannot load TLS credentials: ", err)
+	}
+
+	conn, err := grpc.Dial(
+		"0.0.0.0:50880",
+		grpc.WithTransportCredentials(tlsCredentials),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &client{
+		client: models.NewSSHClient(conn),
+		logger: log.New(os.Stdout, "client: ", log.Ldate|log.Ltime|log.LUTC),
+	}
+}
+
+func (c *client) makeRequest(n int) {
+	resp, err := c.client.Echo(context.Background(), &models.Request{
+		Id:   uint64(n),
+		Body: fmt.Sprintf("client message #%d", n),
+	})
+	if err != nil {
+		c.logger.Printf("ERROR: make request #%d. err: %v", n, err)
+		return
+	}
+	c.logger.Printf("INFO: response server on n=%d: %s", n, resp.Body)
+}
